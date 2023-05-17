5a76e856bea6d55a2562f8e9813a0b3904c1c2dd
victoriasyluma <victoriasyluma3@gmail.com>
2023-05-17 19:47:27 +0200

    adding routes

---
+++ src/pages/HomePage/Home.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+      <p>Welcome to the Home PAge</p>
+    </div>
+  );
+};
---
+++ src/pages/Tweets/Tweets.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export const Tweets = () => {
+  return (
+    <div>
+      <p>Welcome to the tweets</p>
+    </div>
+  );
+};
